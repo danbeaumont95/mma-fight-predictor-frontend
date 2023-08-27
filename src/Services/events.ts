cb6dd7ab1a7beb646e9be500335f5b1b49bca626
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2023-08-27 17:08:14 +0100

    added live api url

--- src/Services/events.ts
+++ src/Services/events.ts
@@ -1,31 +1,32 @@
 import axios from 'axios';
+import { url } from './url'
 
 const getUpcomingEvents = async () => {
-  const res = await axios.get('http://127.0.0.1:8000/mma_fight_predictor/events/');
+  const res = await axios.get(`${url}/mma_fight_predictor/events/`);
   return res;
 };
 
 const getFightsForEvent = async (event: string) => {
-  const res = await axios.post('http://127.0.0.1:8000/mma_fight_predictor/events/get_event_details', {
+  const res = await axios.post(`${url}/mma_fight_predictor/events/get_event_details`, {
     event_url: event,
   });
   return res;
 };
 
 const getBasicFightStatsForFight = async (fight: string) => {
-  const res = await axios.post('http://127.0.0.1:8000/mma_fight_predictor/events/get_basic_fight_stats', {
+  const res = await axios.post(`${url}/mma_fight_predictor/events/get_basic_fight_stats`, {
     fight_url: fight,
   });
   return res;
 };
 
 const getNextEventPoster = async () => {
-  const res = await axios.get('http://127.0.0.1:8000/mma_fight_predictor/events/get_next_event_poster')
+  const res = await axios.get(`${url}/mma_fight_predictor/events/get_next_event_poster`)
   return res
 }
 
 const getInDepthStats = async (fighter_1: string, fighter_2: string) => {
-  const res = await axios.get(`http://127.0.0.1:8000/mma_fight_predictor/events/get_in_depth_stats?fighter_1=${fighter_1}&fighter_2=${fighter_2}`);
+  const res = await axios.get(`${url}/mma_fight_predictor/events/get_in_depth_stats?fighter_1=${fighter_1}&fighter_2=${fighter_2}`);
   return res;
 }
 
--- src/Services/fighters.ts
+++ src/Services/fighters.ts
@@ -1,17 +1,18 @@
 import axios from 'axios';
+import { url } from './url'
 
 const getFighterImage = async (name: string) => {
-  const res = await axios.get(`http://127.0.0.1:8000/mma_fight_predictor/fighter/get_fighter_image?fighter=${name}`);
+  const res = await axios.get(`${url}/mma_fight_predictor/fighter/get_fighter_image?fighter=${name}`);
   return res;
 }
 
 const getMatchupStats = async (name1: string, name2: string) => {
-  const res = await axios.get(`http://127.0.0.1:8000/mma_fight_predictor/fighter/get_stats_for_match_up?fighter_1=${name1}&fighter_2=${name2}`)
+  const res = await axios.get(`${url}/mma_fight_predictor/fighter/get_stats_for_match_up?fighter_1=${name1}&fighter_2=${name2}`)
   return res;
 }
 
 const getMostPopularFightStyles = async () => {
-  const res = await axios.get('http://127.0.0.1:8000/mma_fight_predictor/get_fight_style_with_most_wins');
+  const res = await axios.get(`${url}/mma_fight_predictor/get_fight_style_with_most_wins`);
   return res;
 }
 
